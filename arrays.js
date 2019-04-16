0a06f049c46d5dbc57e038db20732bbb6dae4631
William Eck <billyeck@gmail.com>
2019-04-16 20:40:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,3 +18,25 @@ function destructivelyAddElementToBeginningOfArray(ary, ele){
   ary.unshift(ele)
   return ary
 }
+
+function accessElementInArray(ary, index){
+  return ary[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(ary){
+  ary.shift()
+  return ary
+}
+
+function removeElementFromBeginningOfArray(ary){
+  return ary.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(ary){
+  ary.push()
+  return ary
+}
+
+function removeElementFromEndOfArray(ary){
+  return ary.slice(0, ary.length-1)
+}
